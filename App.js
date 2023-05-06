c206a7fa78ee29689d8d152166a9520d14eba8ad
Sasza Węsierski <alewesij@onet.pl>
2023-05-06 11:13:04 +0200

    Init commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Tytuł taska</Text>
+        <View style={styles.items}></View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    flexWeight: 'boldd',
+  },
+  items: {
+    marginTop: 30,
+  },
+});
