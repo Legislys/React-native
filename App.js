cf2978ce7c7566eb8a630f1229c145c8402abb3a
Sasza Węsierski <alewesij@onet.pl>
2023-05-07 08:50:50 +0200

    Add basic add task component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Keyboard, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import Task from './components/Task.js'
 
 export default function App() {
@@ -7,12 +7,21 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}> Tytuł taska</Text>
-        <Task text='Mleko 1'/>
-        <Task text='Mleko 2'/>
-        <Task text='Milka 1'/>
+        <Task text='Mleko 1' />
+        <Task text='Mleko 2' />
+        <Task text='Milka 1' />
         <Task text='Milka4' />
       </View>
-    </View>
+
+      <KeyboardAvoidingView style={styles.writeTaskWrapper} >
+        <TextInput style={styles.input} placeholder={'Write a task!'} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View >
   );
 }
 
@@ -32,4 +41,31 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText: {}
 })
